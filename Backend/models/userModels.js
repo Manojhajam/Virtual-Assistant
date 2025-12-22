203fdacc1c7baf89bf8b125916230877653062f5
Manoj Hajam <manojhajam4@gmail.com>
2025-12-22 08:57:16 +0545

    env

--- Backend/models/userModels.js
+++ Backend/models/userModels.js
@@ -1,10 +1,27 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
-    }
-})
+      type: String,
+      required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        assistantName: {
+            
+        } 
+  },
+  { timestamps: true }
+);
 
 const user = model("User", userSchema);
+
+export default user;
