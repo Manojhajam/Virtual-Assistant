738211db902e0fe70201fcc4de87db5e8529d01d
Manoj Hajam <manojhajam4@gmail.com>
2025-12-25 09:07:37 +0545

    user authentication

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,27 +1,25 @@
 import genToken from "../config/token.js";
 import User from "../models/userModels.js";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
 export const signUp = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    const existEmail = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-    if (existEmail) {
+    if (!user) {
       return res.status(400).json({
-        message: "Email already exists!", 
+        message: "Email does't exists!",
       });
     }
-    if (password.length < 6) {
+
+    const isMathed = await bcrypt.compare(password, user.password);
+    if (!isMathed) {
       return res.status(400).json({
-        message: "Password must be at least 6 characters!",
+        message: "Password incorrect!",
       });
-      }
-      
-      const hashedPassword = await bcrypt.hash(password, 10)
-      
-    const user = await User.create({ name, hashedPassword, email });
+    }
 
     const token = await genToken(User._id);
 
@@ -29,15 +27,26 @@ export const signUp = async (req, res) => {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
       sameSite: "strict",
-      secure: false
-    })
+      secure: false,
+    });
 
-   return res.status(201).json(user)
+    return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({
-     message: `Sign up error ${error}`
-   })
+      message: `Login error ${error}`,
+    });
   }
 };
 
-
+export const Logout = async (req, res) =>{
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({
+      message: "Log out Successfully!"
+    });
+  } catch (error) {
+   return res.status(500).json({
+      message: `Log out error ${error}`,
+    }); 
+  }
+}
